5b0c8634f3700de095647d8ad1610699b27cd67f
Anjal ghimire <104375352+AnjalLLL@users.noreply.github.com>
2026-04-29 23:26:49 +0545

    Update worker.py

--- messageQueue/worker.py
+++ messageQueue/worker.py
@@ -4,7 +4,7 @@ from rq import Queue
 def hello():
     print("Hello from queue!")
 
-redis_conn = Redis(host="localhost", port=6379, password="Ghimires@123")
+redis_conn = Redis(host="localhost", port=6379, password="own_password")
 queue = Queue("task_queue", connection=redis_conn)
 
 job = queue.enqueue(hello)
